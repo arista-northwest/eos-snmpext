cee7f70838c9476ec42fe05dd3aaf7fae6630c49
Jesse Mather <jmather@arista.com>
2019-08-29 20:20:32 -0700

    target name should be static

--- deploy.py
+++ deploy.py
@@ -6,26 +6,31 @@ import sh
 import sys
 import eapi
 
+target = "snmpext.rpm"
+
 _, local, host = sys.argv[:3]
 
 sess = eapi.session(host)
 
-sh.scp(local, "admin@{}:/tmp/".format(host))
+sh.scp(local, "admin@{}:/tmp/{}".format(host, target))
 
-package = os.path.basename(local)
+#package = os.path.basename(local)
 
-script = """no extension {0}
+script = """configure
+no snmp-server extension .1.3.6.1.4.1.8072.1.3.1.5
+end
+no extension {0}
 delete extension:{0}
-""".format(package)
+delete flash
+""".format(target)
 print(sess.send(script.splitlines()))
 
 script = """copy file:/tmp/{0} extension:
 extension {0}
 copy installed-extensions boot-extensions
 configure
-no snmp-server extension .1.3.6.1.4.1.8072.1.3.1.5
 snmp-server extension .1.3.6.1.4.1.8072.1.3.1.5 file:/var/tmp/snmpext
 end
 write
-""".format(package)
+""".format(target)
 print(sess.send(script.splitlines()))
