ec903284897ef72439344d499df9cee037b15300
Jesse Mather <jmather@arista.com>
2020-01-08 10:38:50 -0800

    update logging

--- eos_snmpext/entry.py
+++ eos_snmpext/entry.py
@@ -7,27 +7,75 @@
 import argparse
 import errno
 import functools
-import importlib
 import os
 import pkgutil
-import re
 import sys
-import syslog
 import time
-# import yaml
+
 import eos_snmpext.extensions
 from eos_snmpext.contrib import snmp_passpersist
-from eos_snmpext.util import memoize
+
+import Logging
+import Tac
 
 # ====================
 BASE_POLLING_INTERVAL = 1
 MAX_RETRY = 10
-BASE_OID = ".1.3.6.1.4.1.8072.1.3.1.5"
-DEBUG = False
+NET_SNMP_EXTEND_OID = ".1.3.6.1.4.1.8072.1.3.1"
+BASE_OID = NET_SNMP_EXTEND_OID + ".5"
 # search these paths for the 'snmpext' directory
 PATHS = ['/mnt/flash', '/persist/local']
-# CONFIG = '/mnt/flash/snmpext-config'
 # ====================
+
+Tac.singleton("Tac::LogManager").syslogFacility = 'logLocal4'
+Logging.logD(id="SYS_SNMPEXT_INFO",
+             severity=Logging.logInfo,
+             format="%s",
+             explanation="[ Informational log message ]",
+             recommendedAction=Logging.NO_ACTION_REQUIRED)
+
+Logging.logD(id="SYS_SNMPEXT_EXTENSION_LOADED",
+             severity=Logging.logInfo,
+             format="%s",
+             explanation="[ ]",
+             recommendedAction=Logging.NO_ACTION_REQUIRED)
+
+Logging.logD(id="SYS_SNMPEXT_EXTENSION_NOTSUPPORTED",
+             severity=Logging.logNotice,
+             format="%s",
+             explanation="[ ]",
+             recommendedAction=Logging.NO_ACTION_REQUIRED)
+
+
+Logging.logD(id="SYS_SNMPEXT_UPDATING",
+             severity=Logging.logInfo,
+             format="%s",
+             explanation="[ ]",
+             recommendedAction=Logging.NO_ACTION_REQUIRED)
+
+Logging.logD(id="SYS_SNMPEXT_SHUTDOWN",
+             severity=Logging.logInfo,
+             format="%s",
+             explanation="[ ]",
+             recommendedAction=Logging.NO_ACTION_REQUIRED)
+
+Logging.logD(id="SYS_SNMPEXT_PIPE_CLOSED",
+             severity=Logging.logError,
+             format="%s",
+             explanation="[ ]",
+             recommendedAction=Logging.NO_ACTION_REQUIRED)
+
+Logging.logD(id="SYS_SNMPEXT_RETRYING",
+             severity=Logging.logWarning,
+             format="%s",
+             explanation="[ ]",
+             recommendedAction=Logging.NO_ACTION_REQUIRED)
+
+Logging.logD(id="SYS_SNMPEXT_RETRIES_EXHAUSTED",
+             severity=Logging.logError,
+             format="%s",
+             explanation="[ ]",
+             recommendedAction=Logging.NO_ACTION_REQUIRED)
 
 PACKAGES = [eos_snmpext.extensions]
 LAST_INTERVAL = {}
@@ -61,15 +109,6 @@ import sys
 sys.stdout = Unbuffered(sys.stdout)
 sys.stderr = Unbuffered(sys.stderr)
 
-def log(pri, msg):
-    syslog.syslog(pri, msg)
-    if DEBUG:
-        print msg
-
-# def _load_config(path):
-#     with open(path, "r") as fh:
-#         return yaml.load(fh.read())
-
 def _load_extensions(names):
     modules = []
     for package in PACKAGES:
@@ -88,7 +127,7 @@ def _load_extensions(names):
             if not is_supported(module):
                 continue
 
-            log(syslog.LOG_INFO, "%SNMPEXT-5-EXT_LOADED: Loaded '{}' snmp extension".format(name))
+            Logging.log(SYS_SNMPEXT_EXTENSION_LOADED, "Loaded extension: %s" % name)
             modules.append(module)
     return modules
 
@@ -96,20 +135,14 @@ def is_supported(extension):
     supported = True
 
     if not hasattr(extension, 'supported'):
-        return supported
+        return True
 
-    try:
-        supported = extension.supported()
-    except Exception as exc:
-        log(syslog.LOG_WARNING, ("%SNMPEXT-4-EXT_FAILED: Extension '{}' "
-             "failed to load: {}").format(extension.__name__, exc.message))
-        supported = False
-
-    if not supported:
-        log(syslog.LOG_INFO, ("%SNMPEXT-5-EXT_UNSUPPORTED: Extension '{}' is "
-             "not supported on this platform").format(extension.__name__))
-
-    return supported
+    if not extension.supported():
+        Logging.log(SYS_SNMPEXT_EXTENSION_NOTSUPPORTED,
+                    "extension '%s' is not supported" % extension.__name__)
+        return False
+    
+    return True
 
 def update(pp, extensions):
     polling_interval = 0
@@ -126,31 +159,18 @@ def update(pp, extensions):
             last_interval = ext._LAST_INTERVAL
 
         if now - last_interval >= polling_interval:
-            log(syslog.LOG_INFO, "%SNMPEXT-6-UPDATING: Polling timer expired, updating {}".format(ext.__name__))
+            Logging.log(SYS_SNMPEXT_UPDATING, "Polling timer expired, updating %s" % ext.__name__)
             ext.update(pp)
             ext._LAST_INTERVAL = now
 
 def main():
-    global DEBUG
 
     parser = argparse.ArgumentParser(prog="arcomm")
     arg = parser.add_argument
 
     arg("extensions", nargs="*", default=[])
-    arg("-d", "--debug", action="store_true", default=False, help="enable debugging")
-    # arg("-c", "--config", default=CONFIG, help="specify config file")
+    
     args = parser.parse_args()
-
-    syslog.openlog('snmpext', 0, syslog.LOG_LOCAL4)
-
-    DEBUG = args.debug
-
-    # config = {}
-    # if os.path.exists(args.config):
-    #     config = _load_config(args.config)
-
-    # if not args.extensions:
-    #     args.extensions = config.get("extensions") or []
 
     extensions = _load_extensions(args.extensions)
     retry_counter = MAX_RETRY
@@ -161,21 +181,26 @@ def main():
             func = functools.partial(update, pp, extensions)
             pp.start(func, BASE_POLLING_INTERVAL)
         except KeyboardInterrupt:
-            log(syslog.LOG_NOTICE, "%SNMPEXT-5-SHUTDOWN: {}".format("Exiting on user request"))
+
+            Logging.log(SYS_SNMPEXT_SHUTDOWN, "Exiting on user request")
             sys.exit(0)
         except IOError as exc:
             if exc.errno == errno.EPIPE:
                 message = "snmpd has closed the pipe"
-                log(syslog.LOG_ERR, "%SNMPEXT-3-PIPE_CLOSED: {}".format(message))
+                Logging.log(SYS_SNMPEXT_PIPE_CLOSED, message)
                 sys.exit(0)
-            message = "updater thread has died: {}".format(exc.message)
-        # except Exception as exc:
-        #     message = "main thread has died: {}".format(str(exc))
+        except EOFError as exc:
+            Logging.log(SYS_SNMPEXT_SHUTDOWN, "EOF. shutting down...")
+            sys.exit(0)
 
-        log(syslog.LOG_WARNING, "%SNMPEXT-4-RETRYING: {}".format(message))
+        Logging.log(SYS_SNMPEXT_RETRYING, message)
+
         retry_counter -= 1
 
-    log(syslog.LOG_ERR, "%SNMPEXT-3-RETRYS_EXHAUSTED: too many retrys, exiting")
+    if retry_counter == 0:
+        Logging.log(SYS_SNMPEXT_RETRIES_EXHAUSTED, "too many retrys, exiting")
+        sys.exit(1)
+    
     sys.exit(0)
 
 if __name__ == "__main__":
