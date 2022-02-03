458e245ae18039bfe4b82bccbaf592c897cc30d0
Jesse Mather <jmather@arista.com>
2022-02-03 14:40:55 -0800

    can't create a sumlink in flash

--- post_install.sh
+++ post_install.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 DIR=/mnt/flash/snmpext
-#mkdir -p $DIR/{bin,extensions}
 mkdir -p $DIR/bin
-#touch $DIR/extentions/__init__.py
-ln -sf /usr/bin/snmpext $DIR/bin/snmpext
+cp -pf /usr/bin/snmpext $DIR/bin/snmpext
 
